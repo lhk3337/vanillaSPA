459b4b8bb2ab74273a4d0bfc43031083396fa745
lhk3337 <lhk3337@naver.com>
2022-02-28 17:43:58 +0900

    fix 메인 페이지 api 적용- #02

--- src/components/ProductListPage.js
+++ src/components/ProductListPage.js
@@ -1,6 +1,5 @@
 export default class ProductListPage {
   constructor({ $target, fetchData }) {
-    console.log(fetchData().data);
     this.$ListPage = document.createElement("div");
     $target.appendChild(this.$ListPage);
     this.$ListPage.className = "ProductListPage";
@@ -14,22 +13,26 @@ export default class ProductListPage {
   }
 
   render() {
-    console.log(this.data);
     const $title = document.createElement("h1");
     const $lists = document.createElement("ul");
 
     $title.innerText = "상품목록";
     this.$ListPage.appendChild($title);
     this.$ListPage.appendChild($lists);
-    $lists.innerHTML = `
-      <li class="Product">
-        <img src="https://grepp-cloudfront.s3.ap-northeast-2.amazonaws.com/programmers_imgs/assignment_image/cafe_coffee_cup.png" />
-        <div class="Product__info">
-        <div>커피잔</div>
-        <div>10,000원~</div>
-      </div>
-      </li>
-      
-      `;
+    $lists.innerHTML = `${this.data
+      .map(
+        (product) => `
+    <li class="Product">
+    <a href=/products/${product.id} >
+    <img src="${product.imageUrl}" />
+    <div class="Product__info">
+    <div>${product.name}</div>
+    <div>${product.price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}원~</div>
+    </div>
+    </a>
+  </li>
+    `
+      )
+      .join("")}`;
   }
 }
