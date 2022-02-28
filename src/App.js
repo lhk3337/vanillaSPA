ca883452df10716dd9f16dee54dc6e200d93adf7
lhk3337 <lhk3337@naver.com>
2022-02-28 22:29:31 +0900

    detail page 추가

--- src/App.js
+++ src/App.js
@@ -22,7 +22,15 @@ export default class App {
         });
       } else if (pathname.indexOf("/products/") === 0) {
         const [, , productId] = pathname.split("/");
-        new ProductDetailPage({ $target, productId });
+
+        const productDetail = new ProductDetailPage({
+          $target,
+          productId,
+          detailFetchData: async () => {
+            const response = await api.fetchDetailProduct(productId);
+            productDetail.setData({ ...response });
+          },
+        });
       } else if (pathname === "/cart") {
         new CartPage({ $target });
       }
--- src/components/ProductDetailPage.js
+++ src/components/ProductDetailPage.js
@@ -1,14 +1,53 @@
 export default class ProductDetailPage {
-  constructor({ $target, productId }) {
-    this.state = { productId };
+  constructor({ $target, productId, detailFetchData }) {
+    this.state = {};
     this.DetailPage = document.createElement("div");
+    this.productDetail = document.createElement("div");
+
     $target.appendChild(this.DetailPage);
+    this.title = document.createElement("h1");
+    this.DetailPage.appendChild(this.title);
+    this.DetailPage.appendChild(this.productDetail);
+
     this.DetailPage.className = "ProductDetailPage";
-    this.render();
+    this.productDetail.className = "ProductDetail";
+    detailFetchData();
   }
-  render() {
-    const title = document.createElement("h1");
-    title.innerText = "상세 페이지";
-    this.DetailPage.appendChild(title);
+  setData(data) {
+    this.data = data;
+    // console.log(this.data);
+    console.log(data);
+    this.render(this.data);
+  }
+  render(data) {
+    const { name, price, imageUrl, productOptions } = data;
+    this.title.innerText = `${name} 상품 정보`;
+    this.productDetail.innerHTML = `
+    <img src=${imageUrl} />
+    <div class="ProductDetail__info">
+      <h2>${name}</h2>
+      <div class="ProductDetail__price">${price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}원~</div>
+      <select>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+      </select>
+      <div class="ProductDetail__selectedOptions">
+        <h3>선택된 상품</h3>
+        <ul>
+          <li>
+            커피잔 100개 번들 10,000원
+            <div><input type="number" value="10" />개</div>
+          </li>
+          <li>
+            커피잔 1000개 번들 15,000원
+            <div><input type="number" value="5" />개</div>
+          </li>
+        </ul>
+        <div class="ProductDetail__totalPrice">175,000원</div>
+        <button class="OrderButton">주문하기</button>
+      </div>
+    </div>
+    `;
   }
 }
