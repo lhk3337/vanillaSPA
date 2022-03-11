a671953c1313776e927dc8778e393ee558c1cdb6
lhk3337 <lhk3337@naver.com>
2022-03-11 17:37:07 +0900

    select tag list 표시 #4

--- client/src/components/ProductDetailPage.js
+++ client/src/components/ProductDetailPage.js
@@ -1,7 +1,5 @@
 export default class ProductDetailPage {
   constructor({ $target, productId, detailFetchData }) {
-    this.state = {};
-
     this.$DetailPage = document.createElement("div");
     this.$productDetail = document.createElement("div");
 
@@ -15,15 +13,19 @@ export default class ProductDetailPage {
     detailFetchData();
   }
   setData(data) {
-    this.data = data;
+    this.state = {
+      product: data,
+      selectedOptions: [],
+    };
     this.render();
   }
-  setSelected(select) {
-    this.select = select;
+
+  setSelected = (select) => {
+    this.state = select;
     this.render();
-  }
+  };
   render() {
-    const { name, price, imageUrl, productOptions } = this.data;
+    const { name, price, imageUrl, productOptions } = this.state.product;
 
     this.$title.innerText = `${name} 상품 정보`;
     this.$productDetail.innerHTML = `
@@ -33,15 +35,14 @@ export default class ProductDetailPage {
       <div class="ProductDetail__price">${price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}원~</div>
       <select id="selectedItem">
         <option selected>선택하세요.</option>
-        ${productOptions.map((product) =>
-          product.stock === 0
-            ? `<option disabled value=${price + product.price}>${product.name} ${
-                product.price !== 0 ? `(+${product.price}원)` : ""
-              }</option>`
-            : `<option value=${price + product.price}>${product.name} ${
-                product.price !== 0 ? `(+${product.price}원)` : ""
-              }</option>`
-        )}
+        ${productOptions
+          .map(
+            (product) =>
+              `<option ${product.stock === 0 ? "disabled" : ""} value=${product.id}>${
+                product.stock === 0 ? "(품절)" : ""
+              }${product.name} ${product.price !== 0 ? `(+${product.price}원)` : ""}</option>`
+          )
+          .join("")}
       </select>
     </div>
     `;
@@ -49,27 +50,51 @@ export default class ProductDetailPage {
     const $selectedOptions = document.createElement("div");
     $selectedOptions.className = "ProductDetail__selectedOptions";
     document.querySelector(".ProductDetail__info").appendChild($selectedOptions);
-
-    const exSelect = document.getElementById("selectedItem");
-    exSelect.addEventListener("change", (e) => {
-      const { value } = e.target;
-      this.setSelected(value);
-    });
-
+    const { product, selectedOptions } = this.state;
+    console.log(this.state);
     $selectedOptions.innerHTML = `
     <h3>선택된 상품</h3>
     <ul>
-      <li>
-        커피잔 100개 번들 ${this.select === undefined ? 0 : this.select}원
-        <div><input type="number" value="10" />개</div>
-      </li>
-      <li>
-        커피잔 1000개 번들 15,000원
-        <div><input type="number" value="5" />개</div>
-      </li>
+    ${selectedOptions
+      .map(
+        (selected) => `
+    <li>
+    ${name} ${selected.optionName} ${this.state === undefined ? 0 : price + selected.optionPrice}원
+    <div><input type="number" value="10" />개</div>
+  </li>
+    `
+      )
+      .join("")}
+   
     </ul>
-    <div class="ProductDetail__totalPrice">175,000원</div>
+    <div class="ProductDetail__totalPrice">0원</div>
     <button class="OrderButton">주문하기</button>
     `;
+
+    const exSelect = document.getElementById("selectedItem");
+    exSelect.addEventListener("change", (e) => {
+      if (e.target.tagName === "SELECT") {
+        const selectedOptionId = parseInt(e.target.value);
+        const { product, selectedOptions } = this.state;
+
+        const option = product.productOptions.find((option) => option.id === selectedOptionId);
+        const selectOption = selectedOptions.find((selectedOption) => selectedOption.optionId === selectedOptionId);
+
+        if (option && !selectOption) {
+          const nextSelectedOptions = [
+            ...selectedOptions,
+            {
+              productId: product.id,
+              optionId: option.id,
+              optionName: option.name,
+              optionPrice: option.price,
+              quantity: 1,
+            },
+          ];
+
+          this.setSelected({ ...this.state, selectedOptions: nextSelectedOptions });
+        }
+      }
+    });
   }
 }
