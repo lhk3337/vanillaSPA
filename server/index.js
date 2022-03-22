79e0c115a6c24cb227c872875c069c69f842e82a
lhk3337 <lhk3337@naver.com>
2022-03-22 20:38:21 +0900

    server try - catch문으로 바꾸기

--- server/index.js
+++ server/index.js
@@ -13,21 +13,31 @@ app.use(express.urlencoded({ extended: true }));
 const API_ENDPOINT = "https://uikt6pohhh.execute-api.ap-northeast-2.amazonaws.com/dev";
 
 app.get("/products", (req, res) => {
-  fetch(`${API_ENDPOINT}/products`).then(async (response) => {
-    const json = await response.json();
-    // console.log(json);
-    res.json(json);
-    res.end();
-  });
+  const getProduct = async () => {
+    try {
+      const response = await fetch(`${API_ENDPOINT}/products`);
+      const json = await response.json();
+      res.json(json);
+      res.end();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getProduct();
 });
 
 app.get("/products/:id", (req, res) => {
-  fetch(`${API_ENDPOINT}/products/${req.params.id}`).then(async (response) => {
-    const json = await response.json();
-    // console.log(json);
-    res.json(json);
-    res.end();
-  });
+  const getDetailProduct = async () => {
+    try {
+      const response = await fetch(`${API_ENDPOINT}/products/${req.params.id}`);
+      const json = await response.json();
+      res.json(json);
+      res.end();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getDetailProduct();
 });
 
 const handleListen = (PORT) => {
