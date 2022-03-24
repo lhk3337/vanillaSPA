63aa6fd1c3474239104422d1288caff7d293d949
lhk3337 <lhk3337@naver.com>
2022-03-24 18:08:51 +0900

    Cart API 적용 및 Cart Component에 보내기 #8

--- client/src/components/Cart/CartPage.js
+++ client/src/components/Cart/CartPage.js
@@ -1,24 +1,57 @@
-import { getItem } from "./storage.js";
-import { routeChange } from "../router.js";
+import { getItem } from "../storage.js";
+import { routeChange } from "../../router.js";
+import { api } from "../../api.js";
+import Cart from "./Cart.js";
+
 export default class CartPage {
   constructor({ $target }) {
+    this.target = $target;
+    this.cartComponent = null;
     this.DetailPage = document.createElement("div");
 
     this.DetailPage.className = "ProductListPage";
     this.DetailPage.innerHTML = `<h1>장바구니</h1>`;
+    this.cartData = getItem("product_cart", []);
     this.state = {
       products: null,
     };
-    this.render($target);
+    this.render();
+    this.fetchProducts();
   }
+  setState(nexState) {
+    this.state = nexState;
+    this.render();
+  }
+  fetchProducts = async () => {
+    const products = await Promise.all(
+      this.cartData.map(async (cartItem) => {
+        const product = await api.fetchCartProduct(cartItem.productId);
+        const selectedOption = product.productOptions.find((option) => option.id === cartItem.optionId);
+        return {
+          imageUrl: product.imageUrl,
+          productName: product.name,
+          quantity: cartItem.quantity,
+          productPrice: product.price,
+          optionName: selectedOption.name,
+          optionPrice: selectedOption.price,
+        };
+      })
+    );
+    this.setState({ products });
+  };
 
-  render($target) {
-    const cartData = getItem("product_cart", []);
-    if (cartData.length === 0) {
+  render() {
+    if (this.cartData.length === 0) {
       alert("장바구니가 비어있습니다.");
       routeChange("/");
     } else {
-      $target.appendChild(this.DetailPage);
+      this.target.appendChild(this.DetailPage);
+      if (this.state.products && !this.cartComponent) {
+        this.cartComponent = new Cart({
+          $target: this.DetailPage,
+          initialState: this.state.products,
+        });
+      }
     }
   }
 }
